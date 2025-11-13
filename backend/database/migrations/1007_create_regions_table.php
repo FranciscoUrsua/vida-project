280148b7aac078740a3cf1bbd9fc9ae0021496de
FranciscoUrsua <francisco.ursua@proton.me>
2025-11-13 12:18:05 +0100

    Solved error in create regions migration

--- backend/database/migrations/1007_create_regions_table.php
+++ backend/database/migrations/1007_create_regions_table.php
@@ -1,1 +1,24 @@
-007_create_regions_table.php
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('regions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('code', 2)->unique();
+            $table->foreignId('country_id')->constrained('countries')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('regions');
+    }
+};
